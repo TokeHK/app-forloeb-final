72674954ab639d388af44f6c908fd3bcdf30be16
TokeHK <toha7@videndjurs.net>
2025-02-03 13:15:03 +0100

    a

--- src/app/cardinfo/page.tsx
+++ src/app/cardinfo/page.tsx
@@ -12,6 +12,9 @@ type RouterParams = {
 const Page = async (props:RouterParams) => {
   const id = props.searchParams.id;
   const data = await getById(id);
+
+  const isText3Array = Array.isArray(data.text3);
+
   return (
     <div className={styles.subpage}>
       {data && 
@@ -19,7 +22,21 @@ const Page = async (props:RouterParams) => {
           <Image src={`/${data.subpageImg}`} alt={data.desc} width={100} height={100} />
           <div>
             <h1>{data.header}</h1>
-            <p>{data.text}</p>
+            <p>{data.text1}</p>
+            <p>{data.text2}</p>
+
+             {isText3Array ? (
+              <ul>
+                {data.text3.map((item:any, index:Number) => (
+                  <li key={data.name + index}>
+                    <Image src={`/${item.img}`} alt={item.email} width={200} height={200} />
+                    <a href={`mailto:${item.email}`}>{item.email}</a>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>{data.text3}</p>
+            )}
           </div>
         </div>
       }
