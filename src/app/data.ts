9c2992310c9673f9db3a103bb769f6bc4f56425b
TokeHK <toha7@videndjurs.net>
2025-02-03 14:07:00 +0100

    a

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,5 +1,6 @@
-export const allData = async () => {
+import { DataType } from "./types/type";
 
+export const allData = async (): Promise<DataType[]> =>  {
   const res = await fetch("http://localhost:3001/getAllData");
   
   if(!res.ok) throw new Error("failed to fetch data");
